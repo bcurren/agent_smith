7d32640b20a66bf4d409c94fe798313028ffdebe
Bernd Ustorf <bernd@outright.com>
2010-07-07 11:28:54 -0700

    expresso works. progress commit.

--- metrics/lib/metrics/initial_engagement.js
+++ metrics/lib/metrics/initial_engagement.js
@@ -1,16 +1,22 @@
-var bm = require('base_metric');
+var bm = require('../../lib/base_metric');
 
 InitialEngagement = function() {};
 
 InitialEngagement.prototype = new bm.BaseMetric;
 InitialEngagement.prototype.constructor = InitialEngagement;
-InitialEngagement.prototype.chartData = function(start_date) {
-  return {
-    "subject_type": "Txn",
-    "subject_id": 10,
-    "event_name": "created",
-    "manual": true
-  }
+InitialEngagement.prototype.chartData = function(start_date, callback) {
+  var return_result = 'foo'
+  db.collection('events', function(err, collection){
+    collection.find(function(err, cursor){
+      return_result = cursor
+    })
+    callback(return_result)
+  // //   // collection.group([], {}, {"count":0}, "function (obj, prev) { prev.count++; }", function(err, results) {
+  // //   //  return_result = results
+  // //   // })
+  })
+  db.close()  
+  sys.log(sys.inspect(return_result))
 };
 
 exports.Metric = InitialEngagement;
--- metrics/test/initial_engagement_test.js
+++ metrics/test/initial_engagement_test.js
@@ -8,58 +8,44 @@ fs = require('fs'),
 mongo = require('../deps/node-mongodb-native/lib/mongodb'),
 svc = require('../lib/service_json');
 
-// require('express');
-// require('express/plugins');
-
-
-
-
-db = new mongo.Db('agent_smith_test', new mongo.Server('localhost', 27017, {}), {});
-
 require('../lib/metrics/initial_engagement')
-ie = new InitialEngagement
-db.open(function(p_db) {
-  configure(function(){
-    set('root', __dirname);
-    set('db', db);
-    use(Static);
-    use(Cookie);
-    use(Logger);
-    
-    db.close() 
-    try {
 
-      exports['test async'] = function(assert, beforeExit){
-        var n = 0;
-        result = ie.chartData("2010-01-01", function(results){})  
-        setTimeout(function(){
-            ++n;
-            assert.ok(true);
-        }, 200);
-        setTimeout(function(){
-            ++n;
-            assert.ok(true);
-        }, 200);  
-        beforeExit(function(){
-          // assert.equal(1, 1, 'this is not right: ' + sys.inspect(result));    
-          // assert.equal(1, result, 'this is not right: ' + sys.inspect(result));
-        });
-      }
-
-    } catch(e) {
-      sys.log(sys.inspect(e))
+exports['test async'] = function(assert, beforeExit){
+    var ie_txn = {
+      "event_name" : "created" , 
+      "manual" : "true" , 
+      "subject_type" : "Txn" , 
+      "subject_id" : "1955276501" , 
+      "user_created_at" : "Mon, 05 Jul 2010 14:04:16 -0700" , 
+      "user_id" : "2084271013" , 
+      "created_at" : "Wed, 07 Jul 2010 14:04:16 -0700" , 
+      "timestamp" : "Tue Jul 06 2010 14:06:22 GMT-0700 (PDT)"
     }
+    var non_ie_txn = {
+      "event_name" : "created" , 
+      "manual" : "true" , 
+      "subject_type" : "Txn" , 
+      "subject_id" : "1955276501" , 
+      "user_created_at" : "Mon, 05 Jul 2010 14:04:16 -0700" , 
+      "user_id" : "2084271013" , 
+      "created_at" : "Wed, 10 Jul 2010 14:04:16 -0700" , 
+      "timestamp" : "Tue Jul 06 2010 14:06:22 GMT-0700 (PDT)"
+    }
+    txns = [ie_txn, non_ie_txn]    
+    db = new mongo.Db('agent_smith_test', new mongo.Server('localhost', 27017, {}), {});
+    var ie = new InitialEngagement  
+    var results = null
+    db.open(function(p_db) {
+      db.collection('events', function(err, collection){
+        collection.insert(ie_txn, function(err, ids) {})
+      })   
+      ie.chartData("2010-01-01", function(txns) {
+        results = txns
+      })        
+      beforeExit(function(){        
+        assert.equal(1, results, results.to_s);
+      });
+      db.close()
+    })
+}
 
-
-
-
-
-
-
-
-
-
-  })
-  
-})
-      
