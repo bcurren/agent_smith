4fa3d3e3f081d4b8760e1c2cd5453cd0231ff85b
Bernd Ustorf <bernd@outright.com>
2010-07-07 11:53:45 -0700

    create ad hoc suite for running asynchronous tests

--- metrics/lib/metrics/initial_engagement.js
+++ metrics/lib/metrics/initial_engagement.js
@@ -5,18 +5,14 @@ InitialEngagement = function() {};
 InitialEngagement.prototype = new bm.BaseMetric;
 InitialEngagement.prototype.constructor = InitialEngagement;
 InitialEngagement.prototype.chartData = function(start_date, callback) {
-  var return_result = 'foo'
-  db.collection('events', function(err, collection){
-    collection.find(function(err, cursor){
-      return_result = cursor
-    })
-    callback(return_result)
-  // //   // collection.group([], {}, {"count":0}, "function (obj, prev) { prev.count++; }", function(err, results) {
-  // //   //  return_result = results
-  // //   // })
+  db.collection('events', function(err, collection){    
+    collection.find(function(err, cursor) {
+      cursor.toArray(function(err, items) {   
+        callback(items)
+      });
+    });
+
   })
-  db.close()  
-  sys.log(sys.inspect(return_result))
 };
 
 exports.Metric = InitialEngagement;
--- metrics/test/initial_engagement_test.js
+++ metrics/test/initial_engagement_test.js
@@ -1,51 +1,97 @@
-require.paths.unshift(__dirname + '../lib');
-require.paths.unshift(__dirname + '../lib/metrics');
-require.paths.unshift(__dirname);
-require.paths.unshift(__dirname + '../deps/express/lib')
+sys = require('sys')
+mongo = require('../deps/node-mongodb-native/lib/mongodb')
+InitialEngagement = require('../lib/metrics/initial_engagement').Metric
 
-sys = require('sys'),
-fs = require('fs'),
-mongo = require('../deps/node-mongodb-native/lib/mongodb'),
-svc = require('../lib/service_json');
 
-require('../lib/metrics/initial_engagement')
+db = new mongo.Db('agent_smith_test', new mongo.Server('localhost', 27017, {}), {});
+var ie = new InitialEngagement
 
-exports['test async'] = function(assert, beforeExit){
-    var ie_txn = {
-      "event_name" : "created" , 
-      "manual" : "true" , 
-      "subject_type" : "Txn" , 
-      "subject_id" : "1955276501" , 
-      "user_created_at" : "Mon, 05 Jul 2010 14:04:16 -0700" , 
-      "user_id" : "2084271013" , 
-      "created_at" : "Wed, 07 Jul 2010 14:04:16 -0700" , 
-      "timestamp" : "Tue Jul 06 2010 14:06:22 GMT-0700 (PDT)"
-    }
-    var non_ie_txn = {
-      "event_name" : "created" , 
-      "manual" : "true" , 
-      "subject_type" : "Txn" , 
-      "subject_id" : "1955276501" , 
-      "user_created_at" : "Mon, 05 Jul 2010 14:04:16 -0700" , 
-      "user_id" : "2084271013" , 
-      "created_at" : "Wed, 10 Jul 2010 14:04:16 -0700" , 
-      "timestamp" : "Tue Jul 06 2010 14:06:22 GMT-0700 (PDT)"
-    }
-    txns = [ie_txn, non_ie_txn]    
-    db = new mongo.Db('agent_smith_test', new mongo.Server('localhost', 27017, {}), {});
-    var ie = new InitialEngagement  
-    var results = null
-    db.open(function(p_db) {
-      db.collection('events', function(err, collection){
-        collection.insert(ie_txn, function(err, ids) {})
-      })   
-      ie.chartData("2010-01-01", function(txns) {
-        results = txns
-      })        
-      beforeExit(function(){        
-        assert.equal(1, results, results.to_s);
-      });
-      db.close()
-    })
+
+var self = this;
+exports['test myfunc'] = function(assert, beforeExit){  
+  var results  
+  __runSuite(function(){
+    ie.chartData("2010-01-01", function(txns) {
+      results = txns
+    })      
+  })  
+  beforeExit(function(){        
+    assert.equal(3, results.length);
+  });  
 }
 
+
+exports['test group'] = function(assert, beforeExit){
+  var bar
+  __runSuite(function(){
+    bar = 'hell yeah'
+  })
+  beforeExit(function(){
+    assert.equal(3, bar)
+  })  
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// PRIVATE
+var __runSuite = function(callback){ 
+  db.open(function(p_db){
+    __setup()
+    callback.call()
+    __teardown()
+    db.close()
+  })
+}
+
+var __setup = function(){  
+  db.collection('events', function(err, collection){
+    collection.insert([ie_txn, non_ie_txn], function(err, ids){})      
+  })  
+}
+
+var __teardown = function(){  
+  db.collection('events', function(err, collection){
+    collection.remove(function(err, collection){})      
+  })      
+}
+
+
+var ie_txn = {
+  "event_name" : "created" , 
+  "manual" : "true" , 
+  "subject_type" : "Txn" , 
+  "subject_id" : "1955276501" , 
+  "user_created_at" : "Mon, 05 Jul 2010 14:04:16 -0700" , 
+  "user_id" : "2084271013" , 
+  "created_at" : "Wed, 07 Jul 2010 14:04:16 -0700" , 
+  "timestamp" : "Tue Jul 06 2010 14:06:22 GMT-0700 (PDT)"
+}
+
+var non_ie_txn = {
+  "event_name" : "created" , 
+  "manual" : "true" , 
+  "subject_type" : "Txn" , 
+  "subject_id" : "1955276501" , 
+  "user_created_at" : "Mon, 05 Jul 2010 14:04:16 -0700" , 
+  "user_id" : "2084271013" , 
+  "created_at" : "Wed, 10 Jul 2010 14:04:16 -0700" , 
+  "timestamp" : "Tue Jul 06 2010 14:06:22 GMT-0700 (PDT)"
+}
+
+    
