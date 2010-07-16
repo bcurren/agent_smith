ade76b67d44e3ce5c1412f225465f1843ff2b6d1
Bernd Ustorf <bernd@outright.com>
2010-07-16 15:06:47 -0700

    move utility methods into base metric

--- metrics/lib/base_metric.js
+++ metrics/lib/base_metric.js
@@ -1,9 +1,38 @@
-BaseMetric = function() {};
+BaseMetric = function() {
+  var self = this
+  this.__formattedDate = function(date){
+    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
+  },
+  this.__thirtyDaysAgoInMillis = function(){
+    if (!self.thirtyDaysAgoInMillis){
+      self.thirtyDaysAgoInMillis =  self.__currentTimeInMillis() - 30 * 24 * 60 * 60 * 1000
+    }
+    return self.thirtyDaysAgoInMillis
+  },
+  this.__currentTimeInMillis = function(){
+    if (!self.currentTimeInMillis){
+      self.currentTimeInMillis = self.__currentTime().getTime() 
+    }
+    return self.currentTimeInMillis
+  },
+  this.__currentTime = function(){
+    if (!self.currentTime){
+      self.currentTime = new Date()
+    }
+    return self.currentTime
+  },
+  this.__thirtyDaysAgo = function(){
+    if (!self.thirtyDaysAgo){
+      self.thirtyDaysAgo = new Date(self.__thirtyDaysAgoInMillis())
+    }
+    return self.thirtyDaysAgo
+  }  
+};
 
 BaseMetric.prototype = {
   chartData: function() {
     throw "Method not implemented!";
-  }
+  } 
 };
 
 exports.BaseMetric = BaseMetric;
--- metrics/lib/metrics/initial_engagement.js
+++ metrics/lib/metrics/initial_engagement.js
@@ -132,36 +132,9 @@ InitialEngagement = function() {
       series: [
         { name: "Manual Txns",        data: manualTxnData },
         { name: "Company Importers",  data: ciData        },
-        { name: "% Engaged Users",              data: userData        },      
+        { name: "% Engaged Users",    data: userData      },      
       ]
     }  
-  },
-  this.__formattedDate = function(date){
-    return date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate()
-  },   
-  this.__thirtyDaysAgoInMillis = function(){
-    if (!self.thirtyDaysAgoInMillis){
-      self.thirtyDaysAgoInMillis =  self.__currentTimeInMillis() - 30 * 24 * 60 * 60 * 1000
-    }
-    return self.thirtyDaysAgoInMillis
-  },
-  this.__currentTimeInMillis = function(){
-    if (!self.currentTimeInMillis){
-      self.currentTimeInMillis = self.__currentTime().getTime() 
-    }
-    return self.currentTimeInMillis
-  },
-  this.__currentTime = function(){
-    if (!self.currentTime){
-      self.currentTime = new Date()
-    }
-    return self.currentTime
-  },
-  this.__thirtyDaysAgo = function(){
-    if (!self.thirtyDaysAgo){
-      self.thirtyDaysAgo = new Date(self.__thirtyDaysAgoInMillis())
-    }
-    return self.thirtyDaysAgo
   },
   this.__divideUserCountsByTotal = function(){
     var userResults = self.results['userResults']
